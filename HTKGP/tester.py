46ca69113da39eecbd1f076401210bbc88f27820
Jerry <127574759+jianruichen@users.noreply.github.com>
2026-01-17 14:54:42 +0800

    Update tester.py

--- HTKGP/tester.py
+++ HTKGP/tester.py
@@ -8,8 +8,6 @@ import torch
 import numpy as np
 from dataset import Dataset
 from scripts import shredFacts
-from de_distmult import DE_DistMult
-from de_transe import DE_TransE
 from de_simple import DE_SimplE
 from measure import Measure
 
